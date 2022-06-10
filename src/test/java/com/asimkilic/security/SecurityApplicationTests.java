df039255cc5b73853a9a3bf9ce997f27c44161b4
asimkilic <a.asim.kilic@gmail.com>
2022-06-10 15:21:31 +0300

    Initialized

---
+++ src/test/java/com/asimkilic/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asimkilic.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
