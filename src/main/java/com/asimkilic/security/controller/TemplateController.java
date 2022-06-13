481726a0ff12dfd7a6eb90468d24cd284351afc0
asimkilic <a.asim.kilic@gmail.com>
2022-06-13 23:19:23 +0300

    form based authentication and redirect after success login added

---
+++ src/main/java/com/asimkilic/security/controller/TemplateController.java
@@ -0,0 +1,20 @@
+package com.asimkilic.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TemplateController {
+    @GetMapping("login")
+    public String getLoginView() {
+        return "login";
+    }
+
+    @GetMapping("courses")
+    public String getCourses() {
+        return "courses";
+    }
+}
